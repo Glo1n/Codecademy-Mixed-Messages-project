dfffb45c53c3863492aeda01df0e6c9987c9c901
Glo1n <92823607+Glo1n@users.noreply.github.com>
2021-11-18 12:29:21 +0100

    add array of names

---
+++ namegenerator.js
@@ -0,0 +1,17 @@
+const names = [
+    "Jezus Christ", "Napoleon Bonaparte", "William Shakespeare", "Abraham Lincoln",
+    "George Washington", "Adolf Hitler", "King Henry VIII", "Charles Darwin", "Queen Elizabeth I",
+    "Karl Marx", "Julius Caesar", "Queen Victoria", "Martin Luther King", "Joseph Stalin",
+    "Albert Einstein", "Christopher Columbus", "Isaac Newton", "Wolfgang Amadeus Mozart", 
+    "King Louis XIV", "Ludwig von Beethoven", "Leonardo da Vinci", "Michelangelo", "Mother Teresa",
+    "Vincent van Gogh", "Rembrandt van Rijn", "Pablo Picasso", "Mark Twain", "Edgar Allan Poe",
+    "Jane Austen", "George Orwell", "Marilyn Monroe", "Nelson Mandela", "John F. Kennedy",
+    "Queen Elizabeth II", "Winston Churchill", "Donald Trump", "Bill Gates", "Muhammed Ali",
+    "Mahatma Gandhi", "Elvis Presley", "Rosa Parks", "Oprah Winfrey", "Desmond Tutu", "Dalai Lama",
+    "Neil Armstrong", "Walt Disney", "Barack Obama", "Malcolm X", "J.K. Rowling", "Angelina Jolie",
+    "John Lennon", "Vladimir Lenin", "Fidel Castro", "Amelia Earhart", "Mary Magdalene", "Micheal Jackson",
+    "Madonna", "Mata Hari", "Cleopatra", "Grace Kelly", "Lionel Messi", "Sigmund Freud", "Kim Kardashian",
+    "J.R.R. Tolkien", "Margaret Thatcher", "Anne Frank", "Queen Marie Antoinette", "Christiano Ronaldo",
+    "Simon Bolivar", "Florence Nightingale", "Marie Curie", "Shakira", "Lance Armstrong", "Brad Pitt",
+    "Sacha Baron Cohen", "Ted Bundy", "Jack the Ripper", "Jeffrey Dahmer", "Charles Manson"
+]
